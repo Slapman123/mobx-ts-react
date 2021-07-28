b5b9b03ea42346f28c4ac2536c7f12ec3e2186bb
Aleksandar Mihajlovic <36760259+Slapman123@users.noreply.github.com>
2021-07-28 02:29:48 +0200

    fix

--- src/components/NewsProfile/NewsProfile.js
+++ src/components/NewsProfile/NewsProfile.js
@@ -2,12 +2,8 @@ import React,{useState,useEffect} from 'react';
 import {useParams} from "react-router-dom";
 import PropTypes from "prop-types";
 import { setup } from "../../utils/setup";
-
 import useFetch from '../../utils/useFetch';
 import ReactMarkdown from "react-markdown"
-
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 
 const  NewsProfile = ({actions,helpers})=>{
     let [fetch,setFetch] = useState(false)
@@ -28,26 +24,6 @@ const  NewsProfile = ({actions,helpers})=>{
       actions.app.setLoading(false)
       actions.app.setError(false)
     } 
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 4
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
-
     return(
         <div className="col">
             <div className="new-profile">
@@ -56,28 +32,17 @@ const  NewsProfile = ({actions,helpers})=>{
                     <ReactMarkdown>{data.content}</ReactMarkdown>
                 </div>
             </div>
-            {
-              fetch?
-              <div className="new-image-holder">
-                  <Carousel 
-                    showDots={false}
-                    swipeable={true}
-                    autoPlay={true}
-                    autoPlaySpeed={2000}
-                    infinite={true}
-                    ssr={true}
-                    arrows={false}
-                    transitionDuration={500}
-                    responsive={responsive}>
-                        { 
-                          data.media.map((logo,i)=>{
-                            return (<div className="slide" key={i}><img src={`http://localhost:1337${logo.url}`}/></div>)
-                          })
-                        }
-                   </Carousel>
-            </div>:null
-
-            }
+            <div className="new-image-holder">
+              {
+                fetch?
+                data.media.map((item,i)=>{
+                  return (
+                     <div className="slide" key={i}>
+                       <img src={`http://localhost:1337${item.url}`}/>
+                     </div>)
+                 }):null
+              }
+            </div>
         </div> 
     )
 }
